a92c4d05714d56819391c43e9456c746a203ecc4
fikrifin <mfikriabdi55@gmail.com>
2025-06-10 09:17:46 +0800

    update kategori

--- transaksi/views/kategoriview.py
+++ transaksi/views/kategoriview.py
@@ -16,7 +16,7 @@ class KategoriViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         # Hanya tampilkan kategori milik user yang sedang login
-        return Kategori.objects.filter(user=self.request.user).order_by('id')
+        return Kategori.objects.filter(user=self.request.user).order_by('nama')
 
     def perform_create(self, serializer):
         # Saat kategori baru dibuat, otomatis set user-nya
