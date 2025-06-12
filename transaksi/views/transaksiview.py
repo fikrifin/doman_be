62cba0fbe401dc4d06baf1defe409309b7672c7d
fikrifin <mfikriabdi55@gmail.com>
2025-06-12 09:04:18 +0800

    update transaksi for donut

--- transaksi/views/transaksiview.py
+++ transaksi/views/transaksiview.py
@@ -1,34 +1,61 @@
 import datetime
 from django.db import transaction
-from django.db.models import Sum, DecimalField
+from django.db.models import Sum, F, Value, DecimalField, CharField
 from django.db.models.functions import Coalesce
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import serializers
+from rest_framework.pagination import PageNumberPagination
 from transaksi.models.rekening import Rekening
 from transaksi.models.transaksi import Transaksi
 from transaksi.serializers.transaksiserializers import TransaksiSerializer
 
+class StandardResultsSetPagination(PageNumberPagination):
+    """
+    Paginate
+    """
+    page_size = 20
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
 class TransaksiViewSet(viewsets.ModelViewSet):
     """
     API endpoint untuk CRUD Transaksi.
-    Mendukung filter berdasarkan bulan dan tahun: /api/transaksi/?bulan=6&tahun=2025
     """
     serializer_class = TransaksiSerializer
     permission_classes = [IsAuthenticated]
+    pagination_class = StandardResultsSetPagination
 
     def get_queryset(self):
         user = self.request.user
         queryset = Transaksi.objects.filter(user=user)
 
-        bulan = self.request.query_params.get('bulan')
-        tahun = self.request.query_params.get('tahun')
-
+        params = self.request.query_params
+        
+        bulan = params.get('bulan')
+        tahun = params.get('tahun')
         if bulan and tahun:
             try:
                 queryset = queryset.filter(tanggal__month=int(bulan), tanggal__year=int(tahun))
+            except (ValueError, TypeError):
+                pass
+        
+        rekening_id = params.get('rekening')
+        if rekening_id:
+            try:
+                queryset = queryset.filter(rekening_id=int(rekening_id))
+            except (ValueError, TypeError):
+                pass
+
+        kategori_id = params.get('kategori')
+        if kategori_id:
+            try:
+                if kategori_id in ['null', '0', '']:
+                    queryset = queryset.filter(kategori__isnull=True)
+                else:
+                    queryset = queryset.filter(kategori_id=int(kategori_id))
             except (ValueError, TypeError):
                 pass
         
@@ -124,8 +151,16 @@ class TransaksiViewSet(viewsets.ModelViewSet):
         pengeluaran_total = semua_transaksi.filter(jenis=Transaksi.Jenis.PENGELUARAN).aggregate(
             total=Coalesce(Sum('jumlah'), 0, output_field=DecimalField())
         )['total']
+        
+        pengeluaran_per_kategori = transaksi_bulan_ini.filter(
+            jenis=Transaksi.Jenis.PENGELUARAN
+        ).values(
+            kategori_nama=Coalesce('kategori__nama', Value('Lainnya'), output_field=CharField())
+        ).annotate(
+            total=Sum('jumlah')
+        ).order_by('-total')
 
-        riwayat_terakhir = transaksi_bulan_ini.order_by('-tanggal', '-dibuat_pada')[:10]
+        riwayat_terakhir = transaksi_bulan_ini.order_by('-tanggal', '-dibuat_pada')[:5]
         riwayat_serializer = self.get_serializer(riwayat_terakhir, many=True)
 
         data = {
@@ -140,6 +175,7 @@ class TransaksiViewSet(viewsets.ModelViewSet):
                 'sisa_saldo': pemasukan_total - pengeluaran_total,
             },
             'riwayat_terakhir': riwayat_serializer.data,
+            'pengeluaran_chart_data': list(pengeluaran_per_kategori)
         }
 
         return Response(data)
